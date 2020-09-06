27316bb24cd29cd7ae57be7eab04c0de5903f25a
Miguel Colom <miguel@mcolom.info>
2020-09-06 21:28:15 +0200

    Added GPL license in sources

--- extract.py
+++ extract.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# lstate (c) 2020 Miguel Colom
+#
 # GNU General Public Licence (GPL)
 #
 # This program is free software; you can redistribute it and/or modify it under
@@ -14,7 +16,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA
-#
+
 
 import argparse
 from xml.etree import ElementTree as ET
--- lstate.c
+++ lstate.c
@@ -7,20 +7,23 @@
 #include "fusion-c/header/io.h"
 #include <stdio.h>
 
-void ensamblador(void)
-{
-__asm
+/*
+lstate (c) 2020 Miguel Colom
 
-ld h,#0
-ld l,#4
-ld b,#0x12
-ld c,#0x16
-//call 0x1234
-jp 0
-ret
+GNU General Public Licence (GPL)
 
-__endasm;
-}
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 void init() {
     // It's mandatory to do this to use files!
