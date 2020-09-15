6606642353711f2ce9cc49429582b2ee866dd095
Miguel Colom <miguel@mcolom.info>
2020-09-15 15:18:16 +0200

    go.sh --> compile.sh

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#cp bestial.stt Fusion-C-v1.2/Working\ Folder/dsk_miguel/
+#cp navy.stt Fusion-C-v1.2/Working\ Folder/dsk_miguel/
+
+cd "Fusion-C-v1.2/Working Folder"
+
+sdcc --nogcse --noinvariant --noinduction --noloopreverse --no-peep      --code-loc 0x180 --data-loc 0x0 --disable-warning 196 -mz80 --no-std-crt0 fusion.lib -L fusion-c/lib/ fusion-c/include/crt0_msxdos_advanced.rel lstate.c && hex2bin -e com lstate.ihx && cp lstate.com dsk_miguel/l.com
+
+echo "openmsx -machine turbor -diska Fusion-C-v1.2/Working\ Folder/dsk_miguel/"
